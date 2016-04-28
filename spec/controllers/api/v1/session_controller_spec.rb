77169f23f4510b1fca59602c860889593a7c2df1
Femi Senjobi <femi.senjobi@andela.com>
2016-04-28 10:36:32 +0100

    test(sessions): write tests for session controller
    write tests for create and deleting user session
    
    [(Start) #[118548727]]

---
+++ spec/controllers/api/v1/session_controller_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+
+describe Api::V1::SessionsController do
+  describe 'POST #create' do
+    before(:each) do
+      @user = FactoryGirl.create(:user)
+    end
+
+    context 'when credentials are correct' do
+      before(:each) do
+        credentials = { email: @user.email, password: '1qw23er45t' }
+        post :create, session: credentials
+      end
+
+      it 'returns the corresponding records' do
+        @user.reload
+        expect(json_response[:email]).to eql @user.email
+      end
+
+      it { should respond_with 200 }
+    end
+
+    context 'when credentials are incorrect' do
+      before(:each) do
+        credentials = { email: @user.email, password: 'qwewqwewqwewq' }
+        post :create, session: credentials
+      end
+
+      it 'returns the corresponding records' do
+        expect(json_response[:error]).to eql 'Invalid login credentials'
+      end
+
+      it { should respond_with 422 }
+    end
+  end
+
+  # describe 'DELETE #destroy' do
+  #   before(:each) do
+  #     @user = FactoryGirl.create(:user)
+  #     sign_in @user
+  #     delete :destroy, id: @user.auth_token
+  #   end
+  #
+  #   it { should respond_with 401 }
+  # end
+end
